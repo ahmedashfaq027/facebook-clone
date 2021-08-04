1c0ba12581d369c901d6a6e93fc7ddeab4a236c8
Ashfaq Mohammed <ahmedashfaq027@gmail.com>
2021-08-04 22:50:11 +0530

    Updates

---
+++ precache-manifest.5bc05927177a9e8807b636b7c1893fad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "434dca90f1828768d95eb673e819517f",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "bf832299a5a367a00396",
+    "url": "/facebook-clone/static/css/main.62bfdb7d.chunk.css"
+  },
+  {
+    "revision": "668a1cc2eba46fde67a2",
+    "url": "/facebook-clone/static/js/2.f3ab38a1.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/facebook-clone/static/js/2.f3ab38a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf832299a5a367a00396",
+    "url": "/facebook-clone/static/js/main.2e0d70c8.chunk.js"
+  },
+  {
+    "revision": "8d9db31eeef904c1dc55",
+    "url": "/facebook-clone/static/js/runtime-main.389e76a3.js"
+  }
+]);
